9ca15539cd76b59d46203408b544c261c019c036
James Stauffer <stauffer.james@gmail.com>
2018-01-05 19:08:15 -0600

    Fixed S3 code.

--- src/main/java/com/productivepathway/liquibase/Lambda.java
+++ src/main/java/com/productivepathway/liquibase/Lambda.java
@@ -10,7 +10,7 @@ import java.io.*;
 import java.sql.*;
 import java.util.*;
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
@@ -30,8 +30,8 @@ public class Lambda implements RequestHandler<Request, Response> {
             String s3Key = request.getPath();//S3 key prefix to directory. Expects connection.properties and changelog.xml
             logger.info("Updating from " + s3Key);
 
-            AmazonS3 s3Client = new AmazonS3Client(new ProfileCredentialsProvider());
-            File downloadDir = new File("download/");
+            AmazonS3 s3Client = new AmazonS3Client(new DefaultAWSCredentialsProviderChain());
+            File downloadDir = new File("/tmp/download");
             downloadDir.mkdirs();
             downloadS3(s3Client, s3Key, downloadDir);
             
@@ -45,55 +45,45 @@ public class Lambda implements RequestHandler<Request, Response> {
 
             response.setSuccess(true);
         } catch (Throwable e) {
-            logger.warn("Unable to process " + request, e);
+            logger.error("Unable to process " + request, e);
             response.addError(e.toString());
         }
         return response;
     }
 
     private static Logger logger = Logger.getLogger(Lambda.class);
-    private final static String BUCKET_NAME = "safetyDb-liquibase";
+    private final static String BUCKET_NAME = "ppi-liquibase";
 
-    private void downloadS3(AmazonS3 s3client, String keyPrefix, File localDirectory) throws IOException {
-        logger.info("Downloading " + keyPrefix + " to " + localDirectory);
-        try {
-            final ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(BUCKET_NAME).withPrefix(keyPrefix);
-            ListObjectsV2Result result;
-            do {               
-                result = s3client.listObjectsV2(req);
-               
-                for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
-                    logger.info("Downloading " + objectSummary.getKey());
-                    S3Object s3Object = s3client.getObject(new GetObjectRequest(BUCKET_NAME, objectSummary.getKey()));
-                    InputStream inputStream = s3Object.getObjectContent();
-                    String localRelativePath = objectSummary.getKey().substring(keyPrefix.length());
-                    File local = new File(localDirectory, localRelativePath);
-                    logger.info("Downloading to " + local);
-                    BufferedOutputStream fos = new BufferedOutputStream(new FileOutputStream(local));
+    private void downloadS3(AmazonS3 s3client, String keyPrefix, File localDirectory) throws IOException, AmazonClientException {
+        logger.info("Downloading " + BUCKET_NAME + ":" + keyPrefix + " to " + localDirectory);
+        final ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(BUCKET_NAME);//.withPrefix(keyPrefix);
+        ListObjectsV2Result result;
+        do {
+            result = s3client.listObjectsV2(req);
 
-                    int read;
-                    byte[] buffer = new byte[4096];
-                    while((read = inputStream.read(buffer, 0, buffer.length)) >= 0) {
-                        fos.write(buffer, 0, read);
-                    }
-                    s3Object.close();
-                    fos.close();
+            for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
+                if(objectSummary.getKey().endsWith("/")) {
+                    logger.info("Skipping folder: " + objectSummary.getKey());
+                    continue;
                 }
-                req.setContinuationToken(result.getNextContinuationToken());
-            } while(result.isTruncated()); 
-            
-        } catch (AmazonServiceException ase) {
-            logger.warn("Unable to download " + keyPrefix, ase);
-/*            System.out.println("Error Message:    " + ase.getMessage());
-            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-            System.out.println("Error Type:       " + ase.getErrorType());
-            System.out.println("Request ID:       " + ase.getRequestId()); */
-        } catch (AmazonClientException ace) {
-            logger.warn("Unable to download " + keyPrefix, ace);
-//            System.out.println("Error Message: " + ace.getMessage());
-        }
-      
+                logger.info("Downloading " + objectSummary.getKey());
+                S3Object s3Object = s3client.getObject(new GetObjectRequest(BUCKET_NAME, objectSummary.getKey()));
+                InputStream inputStream = s3Object.getObjectContent();
+                String localRelativePath = objectSummary.getKey().substring(keyPrefix.length());
+                File local = new File(localDirectory, localRelativePath);
+                logger.info("Downloading to " + local);
+                BufferedOutputStream fos = new BufferedOutputStream(new FileOutputStream(local));
+
+                int read;
+                byte[] buffer = new byte[4096];
+                while((read = inputStream.read(buffer, 0, buffer.length)) >= 0) {
+                    fos.write(buffer, 0, read);
+                }
+                s3Object.close();
+                fos.close();
+            }
+            req.setContinuationToken(result.getNextContinuationToken());
+        } while(result.isTruncated());
     }
 }
 
